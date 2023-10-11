0b410a37643154572672ad17dcd7e9d02cc5ea69
Prapti <praptigoswamislg@gmail.com>
2023-10-11 12:43:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const ColorPicker = () => {
+  const colors = [
+    '#ff0000',
+    '#00ff00',
+    '#0000ff',
+    '#ff8000',
+    '#8000ff',
+    '#00ffff',
+    '#ffff00',
+    '#ff0080',
+    '#0080ff',
+    '#80ff00',
+    '#ff00ff',
+    '#008080',
+    '#808000',
+    '#800080',
+    '#ff80ff',
+    '#80ffff',
+    '#ffff80',
+    '#800000',
+    '#008000',
+    '#000080',
+  ];
+  const [showColors, setShowColors] = useState(false);
+  const [selectedColor, setSelectedColor] = useState('');
+
+  const [innerBoxColor, setInnerBoxColor] = useState('');
+
+  const handleButtonClick = () => {
+    setShowColors(!showColors);
+  };
+
+  const handleColorClick = (color) => {
+    setSelectedColor(color);
+    setShowColors(false);
+    setInnerBoxColor(color);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="color-picker-container">
+      <div className="color-picker-box">
+        <button className="color-picker-button" onClick={handleButtonClick}>
+          Pick a color
+        </button>
+        {showColors && (
+          <div className="color-list">
+            {colors.map((color, index) => (
+              <div
+                key={index}
+                className="color-item"
+                style={{ backgroundColor: color }}
+                onClick={() => handleColorClick(color)}
+              ></div>
+            ))}
+          </div>
+        )}
+        {selectedColor && (
+          <div className="selected-color-box" style={{ backgroundColor: innerBoxColor }}>
+            <p className="selected-color">You picked: {selectedColor}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default ColorPicker;
